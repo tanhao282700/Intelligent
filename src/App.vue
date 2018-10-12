f2d81c64089b6c43097a80b9b4a8c60789f7da78
BlueBlueHang <footballhang@163.com>
2018-10-12 10:48:30 +0800

    1.添加主模块通用样式；

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
 }
 </style>
