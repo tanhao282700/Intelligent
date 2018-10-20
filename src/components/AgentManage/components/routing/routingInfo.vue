728240477490d60c5743db7fc06e92eb11394afc
fanshan0925 <43995687+fanshan0925@users.noreply.github.com>
2018-10-20 19:38:46 +0800

    by fanshan 2018/10/20 代维代码调整

---
+++ src/components/AgentManage/components/routing/routingInfo.vue
@@ -0,0 +1,188 @@
+<!--
+    步骤条下的详细信息
+-->
+<template>
+    <div class="routingTask">    
+      <el-row>
+        <el-col :span="24/data.device.length" v-for="(item,index) in data.device" :key="index">
+          <div class="taskDtl">
+            <el-row :gutter="20">
+              <el-col :span="10"><div class="taskLabel">{{item.label}}</div></el-col>
+              <el-col :span="14"><div class="taskCont">{{item.type}}</div></el-col>
+            </el-row>
+          </div>
+        </el-col>
+      </el-row>  
+      <div>
+        <div class="contLabel">详情描述</div>
+        <el-input
+          type="textarea"
+          class="controlCont controlCont1"
+          :rows="2"
+          placeholder="请输入内容"
+          v-model="data.desc">
+        </el-input>
+      </div> 
+      <div >
+        <div class="contLabel">退回原因</div>
+        <el-input
+          type="textarea"
+          :rows="2"
+          class="controlCont controlCont2"
+          placeholder="请输入内容"
+          v-model="data.backExcuse">
+        </el-input>
+      </div>
+      <div class="dealimg">
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <div>处理前</div>
+          </el-col>
+          <el-col :span="12">
+            <div>处理后</div>
+          </el-col>
+        </el-row>
+        <div>
+          <div class="imgs1">
+            <img :src="item.src" v-for="item in data.imgs1">
+          </div>
+          <Lines :top="0" :hei="100" class="lines"/>
+          <div class="imgs2">
+            <img :src="item.src" v-for="item in data.imgs2">
+          </div>
+        </div>
+      </div>
+      <div class="rightHead" v-if="data.state=='-1'">
+        <span class="infoBusy" v-text="'拒绝退单'"></span>
+        <span class="infoSend" v-text="'允许退单'"></span>
+      </div>
+      <div class="rightHead" v-if="data.state=='0'">
+        <span class="infoBusy" v-text="'拒绝延期'"></span>
+        <span class="infoSend" v-text="'允许延期'"></span>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+      props:['data','device'],
+      data () {
+        return {
+        
+        }
+      },
+      methods:{
+           
+      },
+      created() {
+      },
+      mounted() {
+         if(this.data.device.length>4){
+            $('.taskDtl').css('fontSize','12px')
+         }
+      }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped="" type="text/less"> 
+@import '../../../../assets/css/comon.less';
+.vw(@val){
+  width:@val*100/1366vw;
+}
+.vwMLR(@val){
+  margin: 0 @val*100/1366vw;
+}
+.vwMR(@val){
+  margin-right:@val*100/1366vw;
+}
+.routingTask{
+  width:100%;
+  height:100%;
+  .taskDtl{
+    width:100%;
+    .vhMT(11);
+    padding:0 1.464vw;
+    .taskLabel{
+      color:#4F648B;
+    }
+    .taskCont{
+      color:#B5D7FF;
+    }
+  }
+  .contLabel{
+      .vhLH(54);
+      padding:0 1.464vw;
+      color:#4F648B;
+  }
+  .controlCont{
+      padding:0 1.464vw;
+  }
+  .controlCont1{
+    .vh(50);
+    box-shadow:0px 0px 1px 0px rgba(87,113,176,0.15),0px 1px 2px 0px rgba(0,0,0,0.5);
+    border-radius:1px;
+  }
+  .controlCont2{
+    .vh(70);
+    box-shadow:0px 0px 1px 0px rgba(87,113,176,0.15),0px 1px 2px 0px rgba(0,0,0,0.5);
+    border-radius:1px;
+  }
+  .rightHead{
+    .vw(234);
+    bottom:0;
+    right:0;
+    position: absolute;
+    .vhLH(52);
+    color:#fff;
+    .infoBusy{
+      .vhMT(9);
+      display:inline-block;
+      .vhLH(32);
+      .vw(96);
+      background:#164488;
+      font-size:12px;
+      border-radius:2px;
+      text-align:center;
+      .vwMR(12);
+      &:hover{
+        cursor:pointer;
+      }
+    }
+    .infoSend{
+      .vhMT(9);
+      display:inline-block;
+      .vhLH(32);
+      .vw(96);
+      background:#1989FA;
+      border-radius:2px;
+      font-size:12px;
+      text-align:center;
+      &:hover{
+        cursor:pointer;
+      }
+    }
+  }
+  .dealimg{
+    padding:0 1.464vw;
+    color:#4F648B;
+    .vhLH(34);
+    .lines{
+      float:left;
+      .vwMLR(10)
+    }
+    .imgs1,.imgs2{
+      float:left;
+      .vw(414);
+      .vh(100);
+      white-space: nowrap;
+      overflow:hidden;
+      img{
+        .vw(90);
+        .vh(90);
+        margin:0.22vw;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/AgentManage/components/routing/routingTask.vue
@@ -0,0 +1,181 @@
+<!--
+    steps步骤条封装
+    2018-10-18
+-->
+<template>
+    <div class="routingTask">
+        <div class="routingTime" >
+          <div class="topBox" v-for="(item,index) in data.steps" :style="{left:1.8+(index*160*100/1366)+'vw'}">
+            <div>
+              <span>{{item.name}}</span></br>
+              {{item.date}}&nbsp;&nbsp;&nbsp;{{item.time}}
+            </div>
+            <i class="el-icon-arrow-down"></i>
+          </div>
+        </div>    
+        <div class="progressDiv">
+            <div v-for="(item,index) in data.steps">
+              <img src="../../../../assets/img/AgentManage/point@2x.png" class="item1" :style="{left:6.73+(index*160*100/1366)+'vw'}">
+              <img src="../../../../assets/img/AgentManage/point2@2x.png" class="item2" :style="{left:12.59+(index*160*100/1366)+'vw'}">
+            </div>
+        </div> 
+        <div class="middleTime">
+          <div class="bottomBox" v-for="(item,index) in data.steps" :style="{left:9.31+(index*160*100/1366)+'vw'}">
+            <i class="el-icon-arrow-up"></i>
+            <div>
+              {{item.areatime}}
+            </div>
+          </div>
+        </div>    
+    </div>
+</template>
+
+<script>
+  export default {
+      props:['data'],
+      data () {
+        return {
+
+        }
+      },
+      methods:{
+           
+      },
+      created() {
+      },
+      mounted() {
+         
+      }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped="" type="text/less"> 
+@import '../../../../assets/css/comon.less';
+.vtop(@val){
+  top:@val*100/728vh;
+}
+.vbottom(@val){
+  bottom:@val*100/728vh;
+}
+.vw(@val){
+  width:@val*100/1366vw;
+}
+.vlw(@val){
+  left:@val*100/1366vw;
+}
+.routingTask{
+  width:100%;
+  .vh(164);
+  background:rgba(2,33,74,1);
+  .progressDiv{
+    width:100%;
+    .vh(4);
+    background:#71A6F1;
+    position:relative;
+    box-shadow:0px 3px 1px 0px rgba(181,215,255,0.05);
+    .item1{
+        position:absolute;
+        left:6.73vw;
+        .vtop(-4.4);
+        .vh(12)
+    }
+    .item2{
+        position:absolute;
+        left:12.59vw;
+        .vtop(-3);
+        .vh(10)
+    }
+  }
+  .routingTime{
+    .vh(90);
+    position:relative;
+    color:#B5D7FF;
+    .topBox{
+      background:rgba(4,21,44,1);
+      box-shadow:0px 1px 2px 0px rgba(60,150,255,0.35);
+      position:absolute;
+      border:1px solid rgba(71,137,214,1);
+      .vw(136);
+      .vh(48);
+      font-size:12px;
+      .vhLH(24);
+      left:1.8vw;
+      .vtop(28);
+      text-align:center;
+      span{
+        color:#f9f9f9;
+      }
+      i{
+        position: absolute;
+        left: 53%;
+        color:rgba(71,137,214,1);
+        margin-left: -5px;
+        bottom: -5px;
+        display:block;
+        width:10px;
+        height:5px;
+        z-index: 10;
+        text-align:center;
+        line-height:5px;
+      }
+      i:after{
+        content:'';
+        display:block;
+        position:absolute;
+        .vlw(1.896);
+        top: 0;
+        border-left: 4px solid transparent;
+        border-right: 4px solid transparent;
+        border-top: 4px solid #04152c;
+      }
+    }
+  }
+  .middleTime{
+    .vh(72);
+    position:relative;
+    color:#B5D7FF;
+    .bottomBox{
+      .vw(96);
+      position:absolute;
+      left:9.31vw;
+      top:0;
+      i{
+        position: absolute;
+        left: 53%;
+        color:rgba(255,164,20,1);
+        margin-left: -5px;
+        bottom: -1.3vh;
+        display:block;
+        width:10px;
+        height:5px;
+        z-index: 10;
+        text-align:center;
+        line-height:5px;
+      }
+      i:after{
+        content:'';
+        display:block;
+        position:absolute;
+        left: 0.138vw;
+        top: 0.1vh;
+        border-left: 5px solid transparent;
+        border-right: 5px solid transparent;
+        border-bottom: 4px solid #04152c;
+      }
+      div{
+        position:absolute;
+        .vw(96);
+        .vtop(8);
+        left:0;
+        text-align:center;
+        .vh(35);
+        .vhLH(35);
+        border:0.5px solid rgba(255,164,20,1);
+        box-shadow:0px 4px 8px 0px rgba(243,138,0,0.35);
+        background:rgba(4,21,44,1);
+      }
+    }
+  }
+}
+</style>
