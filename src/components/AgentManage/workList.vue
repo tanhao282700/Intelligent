027f77d7697f78f0120b09c2b55f3b707dfce512
fanshan0925 <43995687+fanshan0925@users.noreply.github.com>
2018-10-20 19:35:48 +0800

    by fanshan 2018/10/20 代维代码调整

--- src/components/AgentManage/workList.vue
+++ src/components/AgentManage/workList.vue
@@ -62,26 +62,59 @@
         </div>
       </div>
       <Dialog wid="546" hei="606" ref="send"> 
-        <SendWork @sendInfosShow="sendInfosShow"/>
+        <SendWork @sendInfosShow="sendInfosShow"/> <!-- 派单 -->
       </Dialog> 
       <Dialog wid="1326" hei="640" ref="dialog">
-        <WorkInfo @tableInfos2Show="tableInfos2Show"/>
+        <WorkInfo @tableInfos2Show="tableInfos2Show" :dtlObj="dtlObj"/>
       </Dialog> 
       <Dialog wid="910" hei="686" ref="tableInfos2">
         <div class="tableInfos">
           <div class="infoHead">
             <span class="infoName" v-text="infoItem.name"></span>
             <span class="infoState" v-text="this.infoTit(infoItem.state)"></span>
-            <span class="infoType" v-text="infoItem.sType"></span>
+          </div>
+          <div class="rightHead">
+            <span class="infoBusy" v-text="'普通'"></span>
+            <span class="infoSend" v-text="infoItem.sType" v-show="true"></span>
+            <span class="infoPer" v-text="'人工派发'" v-show="!true"></span>
           </div>
           <div class="infoWater">
-
+              <RoutingTask :data="infoItem"></RoutingTask>
           </div>
           <div class="infoBoxs">
-
+              <RoutingInfo :data="infoItem"/>
           </div>
         </div>
-      </Dialog>     
+      </Dialog>  
+      <Dialog wid="364" hei="216" ref="isRefult"><!-- 同意退单 -->
+          <div v-text="dialogBoxs.txt" class="isRefTxt"></div>
+          <div class="isRbtnBoxs">
+              <span @click="submitOk">确定</span>
+              <span @click="submitNo">取消</span>
+          </div>
+      </Dialog> 
+      <Dialog wid="414" hei="256" ref="sendWork2"><!-- 重新选择工单处理人员 -->
+          <div class="sendWork2">
+              <div class="oldName">
+                  <label for="">原处理人员：</label>
+                  <span class="namess" v-text="dialogBoxs.item.name"></span>
+              </div>
+              <div class="newName">
+                  <label for="">新工单处理人员：</label>
+                  <div class="ChooseBox">
+                      <SelectBox
+                          :options = 'names' 
+                          :value = "vName" 
+                          placeholder='姓名'
+                          @change = 'change2'
+                      />
+                  </div>
+              </div>
+          </div>
+          <div class="sendWork2Boxs btnBai1" @click="sendWork2">
+              <span>确定</span>
+          </div>
+      </Dialog>    
   </div>
 </template>
 
@@ -94,15 +127,20 @@ import TimePickerT from './components/work/timePickerTit2';
 import Percentage from './components/work/Percentage';
 import WorkInfo from './components/work/workInfo';
 import SendWork from './components/work/sendWork';
-
+import State from './components/work/state';
+import deal from './components/work/deal';
 import Table from '@/components/common/table';
+import RoutingTask from './components/routing/routingTask'
+import RoutingInfo from './components/routing/routingInfo'
 export default {
   components:{
     'Table':Table,
     'WorkInfo':WorkInfo,
     'SelectBox':SelectBox,
     'TimePickerT':TimePickerT,
-    'SendWork':SendWork
+    'SendWork':SendWork,
+    'RoutingTask':RoutingTask,
+    'RoutingInfo':RoutingInfo
   },
   data () {
     return {
@@ -128,6 +166,118 @@ export default {
           {label:'白居易',value:4},
           {label:'狗蛋儿',value:5},
         ],
+        dialogBoxs:{
+            item:{name:''},
+            state0:0, //1 同意，0拒绝
+            txt:'是否允许退单'
+        },
+        dtlObj:{
+          title:'工单详情',
+          value7:'7-20',
+          tabs:[{
+            'name':'今日工单总数',
+            num:78
+          },{
+            'name':'已完成',
+            num:65
+          },{
+            'name':'未完成',
+            num:13
+          }],
+          tableDtl:{
+              hei:488, //table高度  设置后有滚动条
+              len:800, //总条数
+              data:[
+                  {id:1,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:-1,desc:'范珊看哪个阶段呢是范珊的咖啡店给你的肌肤喝点水放辣椒是非得失，反倒是',backExcuse:'范珊看哪个阶段呢是范珊的咖啡店给你的肌肤喝点水放辣椒是非得失，反倒是',
+                  imgs1:[{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    }],
+                    imgs2:[{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    },{
+                      src:'../../../../assets/img/logo.png'
+                    }],
+                  steps:[{
+                    name:'派发时间',date:'2018/09/14',time:'09:13',areatime:'3h',
+                  },{
+                    name:'接单时间',date:'2018/09/16',time:'09:13',areatime:'48h'
+                  },{
+                    name:'派发时间',date:'2018/09/16',time:'11:15',areatime:'2h'
+                  }],
+                  device:[{
+                    label:'类型',
+                    type:'给排水系统'
+                  },{
+                    label:'设备类型',
+                    type:'给排水系统'
+                  },{
+                    label:'设备地点',
+                    type:'这边字宽130PX'
+                  },{
+                    label:'工单处理人员',
+                    type:'李宏源'
+                  }],
+                }, 
+                  {id:2,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:0}, 
+                  {id:3,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:1}, 
+                  {id:4,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:2}, 
+                  {id:5,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:3}, 
+                  {id:6,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:3}, 
+                  {id:7,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:3}, 
+                  {id:8,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:3}, 
+                  {id:9,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:3}, 
+                  {id:10,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:3}, 
+                  {id:11,u_id:1,name:'白狗汪1',type:'给排水系统',addr:'青羊区工业园区T区8栋9楼',eName:'二号会议室照明系统',sTime:'2018-08-27 11:35',info:'几水泵房水泵温度异',sType:'系统派发',state:3},                       
+              ],
+              th:[
+                {prop:'name',label:'名称'},
+                {prop:'type',label:'类别'},
+                {prop:'addr',label:'地点',wid:208}, 
+                {prop:'eName',label:'设备名称',wid:169},
+                {prop:'sTime',label:'派发时间',wid:165},
+                {prop:'info',label:'内容描述',wid:194},
+                {prop:'sType',label:'派发类别'},
+                {prop:'states',label:'状态',wid:146,
+                  operate: true, 
+                    render: (h, param)=> {
+                        const btnss = {
+                            fills:param.row.state,  
+                        };
+                        return h(State,{
+                        props: { state:btnss},
+                        on:{}
+                        });
+                    }},
+                {prop:'fill',label:'操作',wid:105,
+                  operate: true, 
+                    render: (h, param)=> {
+                        const btnss = {
+                            item:param.row, 
+                          //   id:param.row.id,  
+                        };
+                        return h(deal,{
+                        props: { btnss:btnss},
+                        on:{agree:this.agree,refult:this.refult}
+                        });
+                    } 
+                },
+              ]
+          }
+        },
         vName:-1,
         //日期选择
         value7:'8-24',
@@ -236,6 +386,54 @@ export default {
         this.$refs.dialog.show();
         // console.log(row);
       },
+      agree(item){ //同意
+        let state = item.state;
+        if(state==-1){ //退单
+            this.dialogBoxs = {
+                item:item,
+                state0:1,
+                txt:'是否允许退单'
+            };
+        }else if(state==0){ //申请延期处理
+            this.dialogBoxs = {
+                item:item,
+                state0:1,
+                txt:'是否允许延期处理'
+            };
+        } 
+        this.$refs.isRefult.show();
+      },
+      refult(item){//拒绝
+        let state = item.state;
+        if(state==-1){ //退单
+            this.dialogBoxs = {
+                item:item,
+                state0:0,
+                txt:'是否拒绝退单'
+            };
+        }else if(state==0){ //申请延期处理
+            this.dialogBoxs = {
+                item:item,
+                state0:0,
+                txt:'是否拒绝延期处理'
+            };
+        } 
+        this.$refs.isRefult.show();
+      },
+      submitOk(){ //确认
+        //console.log(this.dialogBoxs);// 延期则直接关闭
+        this.$refs.isRefult.hide();
+        if(this.dialogBoxs.item.state==-1){//退单之后，弹出新的工单分配框
+            this.$refs.sendWork2.show();
+        }
+        
+      },
+      submitNo(){ //取消
+        this.$refs.isRefult.hide();
+      },
+      sendWork2(){ //重新派发工单
+        this.$refs.sendWork2.hide();
+      },
       sendWork(){
         this.$refs.send.show();
       },
@@ -243,7 +441,6 @@ export default {
 
       },
       tableInfos2Show(item){
-        console.log(item)
         this.infoItem = item;
         this.$refs.tableInfos2.show();
       },
@@ -282,6 +479,7 @@ export default {
 @import '../../assets/css/comon.less';
 
 .workBox{
+  width:100%;height:100%;
   .workHead{
     width: 95.6vw;
     .vh(108);
@@ -433,6 +631,111 @@ export default {
         font-size: 0.12rem;
       }
     }
+    .rightHead{
+        position: absolute;
+        right: 0;
+        .vhLH(52);
+        color:#fff;
+        top:0;
+        width: 12.59vw;
+        .infoBusy{
+          .vhMT(9);
+          display:inline-block;
+          .vhLH(24);
+          background:#008AFF;
+          width:4.98vw;
+          font-size:12px;
+          border-radius:2px;
+          text-align:center;
+        }
+        .infoSend{
+          .vhMT(9);
+          display:inline-block;
+          .vhLH(24);
+          background:#FA6074;
+          width:4.98vw;
+          border-radius:2px;
+          font-size:12px;
+          text-align:center;
+        }
+        .infoPer{
+          .vhMT(9);
+          display:inline-block;
+          .vhLH(24);
+          background:#F38A00;
+          width:4.98vw;
+          border-radius:2px;
+          font-size:12px;
+          text-align:center;
+        }
+      }
   }
+  .sendWork2{
+      .vh(197);
+      width: 100%;
+      .vhPT(13);
+      padding-left: 0.20rem;
+      .oldName,.newName{
+          width: 100%;
+          font-size: 0.14rem;
+          label{
+              .vh(32);
+              .vhLH(32); 
+              color: #4f648b;   
+          }
+          span.namess{
+              color: #ffa414;
+          }
+          .ChooseBox{
+              .vh(40);
+              width: 2.24rem;
+              background-color: rgba(255, 255, 255, 0.01);
+              border-radius: 0.04rem;
+              border: solid 1px #1989fa;
+              .vhMT(7);
+              span{
+                  font-size: 0.14rem !important;
+                  color: green!important;
+              }
+          }            
+      }
+    }
+    .sendWork2Boxs{
+        width: 3.84rem;
+        .vh(43);
+        margin-left: 0.15rem;
+    }
 }
+.isRefTxt{
+    .vh(170);
+    width: 100%;
+    text-align: center;
+    font-size: 0.18rem;
+    color: #b5d7ff;
+    .vhLH(145);
+}
+.isRbtnBoxs{
+    width: 100%;
+    .vh(45);
+    display:flex;
+    overflow: hidden;
+    border: 0.01rem solid #4a90e2;
+    border-bottom-left-radius: 0.08rem;
+    border-bottom-right-radius: 0.08rem;
+    span{
+        flex: 1;
+        text-align: center;
+        .vhLH(45);            
+        font-size: 0.16rem;
+        color: #fff;
+        cursor: pointer;
+        &:nth-child(1){
+            border-right: 0.01rem solid #4a90e2;
+        }
+        &:active{
+            background: #3b85ef;
+        }
+    }
+}
+
 </style>
