80dd1ab10b3bb333078f1c11a38d7874898da00a
th <tanhao_282700@163.com>
2018-10-20 14:49:28 +0800

    接口代理配置

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'https://tesing.china-tillage.com',
+        changeOrigin: true,
+        secure:true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
