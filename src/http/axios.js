35cd11786705edc75aa773f5e4771a2b9e968aa4
BlueBlueHang <footballhang@163.com>
2018-10-25 10:51:23 +0800

    1.修改baseurl,现在可直接跨域请求；

--- src/http/axios.js
+++ src/http/axios.js
@@ -3,14 +3,15 @@
 import axios from 'axios'
 import qs from 'qs'
 
-const debug = process.env.NODE_ENV != 'production'
+/*const debug = process.env.NODE_ENV != 'production'
 let baseurl = ''
 if(debug){
   baseurl = './api'
 }else{
   baseurl = 'https://tesing.china-tillage.com'
-}
-/*baseurl = 'https://tesing.china-tillage.com'*/
+}*/
+
+let baseurl = 'https://tesing.china-tillage.com';
 
 /*axios.interceptors.request.use(config => {    // 这里的config包含每次请求的内容
   // 判断localStorage中是否存在api_token
