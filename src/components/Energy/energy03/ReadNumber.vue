f0f0f693dd2904988e852ae0269662f992480a29
th <tanhao_282700@163.com>
2018-10-27 02:18:42 +0800

    三表远抄

---
+++ src/components/Energy/energy03/ReadNumber.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="readNumber">
+    <div class="head">
+      <div class="type">
+        <span class="active">区域
+          <div class="spacing">
+            <div class="to-top"></div>
+            <div class="to-bottom"></div>
+          </div>
+        </span>
+        <span>系统</span>
+      </div>
+      <!--<div class=""></div>-->
+    </div>
+    <div class="con">
+      <div class="parts">
+        <div class="part boxs"></div>
+        <div class="part boxs"></div>
+      </div>
+      <div class="parts">
+        <div class="part boxs"></div>
+        <div class="part boxs"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+  export default{
+    name:'ReadNumber',
+    data(){
+      return{
+
+      }
+    },
+    components:{
+
+    },
+    watch:{
+
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+  @import '../../../assets/css/comon.less';
+  .readNumber{
+    width:100%;
+    height:100%;
+    .head{
+      height:10%;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      position:relative;
+      .type{
+        width:30%;
+        height:55.17%;
+        border:1px solid #3a84ef;
+        border-radius:2px;
+        display: flex;
+        flex-direction: row;
+        span{
+          flex:1;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          color:#1989f9;
+          font-size:.16rem;
+          position:relative;
+          &:hover{
+            cursor: pointer;
+          }
+          .spacing{
+            position:absolute;
+            right:0;
+            top:0;
+            width:1px;
+            height:100%;
+            .to-top{
+              width:2px;
+              height:50%;
+              .gradientT(rgba(73,143,226,0.5),rgba(73,143,226,0));
+            }
+            .to-bottom{
+              width:2px;
+              height:50%;
+              .gradientT(rgba(73,143,226,0),rgba(73,143,226,0.5));
+            }
+          }
+        }
+        .active{
+          background:rgba(0, 0, 0, 0.2);
+        }
+      }
+    }
+    .con{
+      height:90%;
+      display: flex;
+      flex-direction: column;
+      .parts{
+        height:46.15%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        &:last-child{
+          margin-top:1.6%;
+        }
+        .part{
+          flex:1;
+          &:last-child{
+            margin-left:1.6%;
+          }
+        }
+      }
+    }
+  }
+</style>
--- src/components/Energy/energy03/metersRead.vue
+++ src/components/Energy/energy03/metersRead.vue
@@ -1,14 +1,90 @@
 <template>
   <!-- 三表远抄模块 -->
+  <div class="meterCopy">
+    <!--面包屑-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>能源管理系统</el-breadcrumb-item>
+      <el-breadcrumb-item>三表远抄</el-breadcrumb-item>
+    </el-breadcrumb>
 
+    <!--内容区域-->
+    <div class="contentBox">
+      <div class="meterNav">
+        <span @click="changeView('ReadNumber')" :class="{'active':viewPath=='ReadNumber'}">实时读数</span>
+        <span @click="changeView('History')" :class="{'active':viewPath=='History'}">历史记录录入</span>
+      </div>
+      <div class="meterCon boxs">
+        <component :is="viewPath"></component>
+      </div>
+    </div>
+
+
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "meter-read"
-    }
+  import ReadNumber from './ReadNumber.vue'
+  import History from './History.vue'
+  export default {
+    name: 'meterCopy',
+    components: {ReadNumber,History},
+    data () {
+      return {
+        viewPath:'ReadNumber'
+      }
+    },
+    methods:{
+      changeView(path){
+          this.viewPath = path
+      }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+  }
 </script>
 
-<style scoped>
-
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped rel="stylesheet/less">
+  @import '../../../assets/css/comon.less';
+  .meterCopy{
+    width:100%;
+    height:100%;
+    padding:0 30px 50px 30px;
+    .contentBox{
+      display: flex;
+      flex-direction: column;
+      .meterNav{
+        height:40px;
+        display: flex;
+        flex-direction: row;
+        span{
+          color:#777b86;
+          font-size:.16rem;
+          display: flex;
+          align-items: center;
+          &:last-child{
+            margin-left:24px;
+          }
+          &:hover{
+            cursor: pointer;
+          }
+        }
+        .active{
+          color:#0064ba;
+          border-bottom:2px solid #0064ba;
+          padding-top:2px;
+        }
+      }
+      .meterCon{
+        flex:1;
+        padding:0 18px;
+      }
+    }
+  }
 </style>
