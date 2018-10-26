41f06ee76554a0121495f2973d42b266287a9474
BlueBlueHang <footballhang@163.com>
2018-10-26 17:08:30 +0800

    1.权限列表子模块表格中增加序号；

--- src/assets/global/base.vue
+++ src/assets/global/base.vue
@@ -1,15 +1,11 @@
 <script>
   import axios from 'axios';
 
-  axios.defaults.timeout = 10000;
-
-  /*axios.defaults.baseURL = 'https://tesing.china-tillage.com';
-  axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-  axios.defaults.headers.post['access-control-allow-origin'] = '*';
-  axios.defaults.headers.common['Authorization'] = 'password_projectid_userid';*/
-
   export default {
-    name:'base'
+    name:'base',
+    mounted(){
+
+    }
   }
 </script>
 
