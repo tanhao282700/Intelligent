4ccb6b7c09c6b45434bc0c9f4df2a19b124a898a
yyys <yangshuzhong6@163.com>
2018-10-28 11:47:10 +0800

    10-28
    用能报表部分样式提交

---
+++ src/components/Energy/energy05/components/reportLeader.vue
@@ -0,0 +1,122 @@
+<template>
+    <!-- 用能预设模块 -->
+    <div class="reportTabBoxs">
+    	<div class="reportTabBoxsHead">
+	        <div class="dateBox">
+	            <el-date-picker v-model="releasetime1" type="date" placeholder="日"></el-date-picker>
+	        </div>
+	        <button class="btn">查询</button>
+	        <button class="btn btnExport floatRt">导出</button>
+    	</div>
+    	<div class="reportTablesBox reportLeader">
+
+	    	<el-table  :data="tableData3"  :cell-class-name="cell"
+	    			   :summary-method="getSummaries" 
+	    			   show-summary>
+	    		<el-table-column type="index" label="序号">
+	    		</el-table-column>
+	    		<el-table-column prop="date" label="名称">
+	    			<el-table-column prop="date" label="指标">
+	    			</el-table-column>
+	    		</el-table-column>
+	    		<el-table-column  label="合计">
+	    			<el-table-column prop="city" label="去年昨天">
+	    			</el-table-column>
+	    			<el-table-column prop="city" label="上一天">
+	    			</el-table-column>
+	    			<el-table-column prop="city" label="昨天">
+	    			</el-table-column>
+	    		</el-table-column>
+	    		<el-table-column  label="客房">
+	    			<el-table-column prop="city" label="去年昨天">
+	    			</el-table-column>
+	    			<el-table-column prop="city" label="上一天">
+	    			</el-table-column>
+	    			<el-table-column prop="city" label="昨天">
+	    			</el-table-column>
+	    		</el-table-column>
+	    		<el-table-column  label="餐饮">
+	    			<el-table-column prop="city" label="去年昨天">
+	    			</el-table-column>
+	    			<el-table-column prop="city" label="上一天">
+	    			</el-table-column>
+	    			<el-table-column prop="city" label="昨天">
+	    			</el-table-column>
+	    		</el-table-column>
+	    	</el-table>
+    		
+    	</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+        	return{
+        		releasetime1:'',
+        		tableData3: [{
+		          city: 4,
+		        }, {
+		          city: 4,
+		        }, {
+		          city: 4,
+		        }, {
+		          city: 4,
+		        }, {
+		          city: 4,
+		        }, {
+		          city: 4,
+		        }, {
+		          city: 4,
+		        }]
+        	}
+        },
+        methods:{
+		    cell({row, column, rowIndex, columnIndex}) {
+		      //加 类
+		        if(columnIndex == 2 || columnIndex == 3 || columnIndex == 4 ||columnIndex == 5 ||columnIndex == 6 || columnIndex == 7 || columnIndex == 8 ||columnIndex == 9 ||columnIndex == 10 ){
+		        	return 'darkOr'
+
+		        }
+		    },
+        	getSummaries(param) {
+		        const { columns, data } = param;
+		        const sums = [];
+		        columns.forEach((column, index) => {
+		        	console.log(column);
+		        	console.log(index);
+		          if (index === 0) {
+		            sums[index] = '13';
+		            return;
+		          }else if(index === 1){
+		            sums[index] = '营业收入总额 (万元)';
+		            return;
+		          }
+		          if(index == 2 || index == 3 || index == 4){
+		          	 column.colSpan = 3;
+		          }
+		          const values = data.map(item => Number(item[column.property]));
+		          if (!values.every(value => isNaN(value))) {
+		            sums[index] = values.reduce((prev, curr) => {
+		              const value = Number(curr);
+		              if (!isNaN(value)) {
+		                return prev + curr;
+		              } else {
+		                return prev;
+		              }
+		            }, 0);
+		            sums[index] += ' ';
+		          } else {
+		            sums[index] = 'N/A';
+		          }
+		        });
+
+		        return sums;
+		    },
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/components/Energy/energy05/energyReport.vue
+++ src/components/Energy/energy05/energyReport.vue
@@ -1,14 +1,94 @@
 <template>
     <!-- 用能报表模块 -->
+    <div class="energyReport">
+    	<div class="navCrumbs">首页 > 能源管理系统 > <span>用能报表</span></div>
+		<div class="reportTabBox">
+        	<span class="unitPriceBtn" @click="unitShowCli">单价预设</span>
+	        <nav class="tabNav">
+	            <span><a :class="isActive1==true?'on':''" @click="toggleTabs(first)">月报表</a></span>
+	            <span><a :class="isActive2==true?'on':''" @click="toggleTabs(second)">日报表</a></span>
+	            <span><a :class="isActive3==true?'on':''" @click="toggleTabs(third)">能源汇总账单 (领导)</a></span>
+	        </nav>
+	        <tabs :is="currentView" keep-alive></tabs>
+		</div>
 
+		<unit v-on:unitShowBool="unitShowBool" v-show="unitShow"></unit>
+	
+    </div>
 </template>
 
 <script>
+
+	import reportDay from './components/reportDay.vue'
+	import reportMonth from './components/reportMonth.vue'
+	import reportLeader from './components/reportLeader.vue'
+	import unitPrice from './components/unitPrice.vue'
+
     export default {
-        name: "energy-report"
+        components:{
+            'first':reportMonth,
+            'second':reportDay,
+            'third':reportLeader,
+            'unit':unitPrice
+        },
+	    data() {
+	        return {
+	        	unitShow:false,
+	        	//tab切换状态
+                first:'first',
+                second:'second',
+                third:'third',
+                currentView:'first',
+                isActive1:true,
+                isActive2:false,
+                isActive3:false,
+	        };
+	    },
+        mounted(){
+            this.getData();
+        },
+	    methods: {
+            getData(){
+                var that = this;
+                this.$http.post('/video_monitoring/video_index_view',{
+                    sys_menu_id:13,
+                    floor_id:115,
+                }).then(function(data){
+                    //响应成功回调
+                    console.log(data);
+
+                }, function(data){
+                    // 响应错误回调
+                });
+            },
+            unitShowCli(){
+            	this.unitShow = true;
+            },
+            unitShowBool(val){
+            	this.unitShow = val;
+            },
+            toggleTabs(tabText){
+                if(tabText == "first"){
+                    this.isActive2 = false;
+                    this.isActive3 = false;
+                    this.isActive1 = true;
+                }else if(tabText == "second"){
+                    this.isActive2 = true;
+                    this.isActive1 = false;
+                    this.isActive3 = false;
+                }else if(tabText == "third"){
+                    this.isActive3 = true;
+                    this.isActive1 = false;
+                    this.isActive2 = false;
+                }
+                this.currentView = tabText;
+            }
+	    }
     }
 </script>
 
-<style scoped>
+<style>
+
+	@import '../../../assets/css/doorControl.css' ;
 
 </style>
