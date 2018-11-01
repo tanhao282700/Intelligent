36fbfe910d82445c064f6d74dd39075a5f3b82da
yyys <yangshuzhong6@163.com>
2018-11-01 11:10:33 +0800

    lou

--- src/components/HotelStatus/components/HotelRoomStatus.vue
+++ src/components/HotelStatus/components/HotelRoomStatus.vue
@@ -16,7 +16,7 @@
     			<div class="roomListHeadBox">
     				<span>酒店房态表 <i></i></span>
     				<span v-for="item in roomListsStaTit" :class="item.sta"> <i></i>{{item.labName}}</span>
-		            <el-select v-model="floorNumber" placeholder="楼号">
+		            <el-select v-model="floorNumber" placeholder="楼号" @change="getData">
 		                <el-option label="1楼" value="1"></el-option>
                         <el-option label="2楼" value="2"></el-option>
 		                <el-option label="3楼" value="3"></el-option>
@@ -86,10 +86,10 @@
 	        };
 	    },
         mounted(){
-            this.getData()
+            this.getData("");
         },
 	    methods: {
- 			getData(){
+ 			getData(selVal){
                 var that = this;
                 this.$http.post('/hotel/hotel_state',{
                     // sys_menu_id:15,
@@ -97,7 +97,7 @@
                     // floor_id:104,
                 }).then(function(data){
                     // 响应成功回调
-                    // console.log(data.data.data);
+                    console.log(data.data.data);
                     // that.tableData = response.data.data.entrance_guard_record;
                     that.hotelMainInfo[0].labVal = data.data.data.rooms;
                     that.hotelMainInfo[1].labVal = data.data.data.room_reservation;
@@ -107,9 +107,63 @@
                     that.hotelMainInfo[5].labVal = data.data.data.vacancy_rate + '%';
                     that.hotelMainInfo[6].labVal = data.data.data.diners;
 
-                    that.roomsType[0].roomsLst = data.data.data.separate_room;
-                    that.roomsType[1].roomsLst = data.data.data.standard_room;
-                    that.roomsType[2].roomsLst = data.data.data.suite;
+                    var newArrDj1 = [];
+                    var newArrDj2 = [];
+                    var newArrDj3 = [];
+                    var newArrBj1 = [];
+                    var newArrBj2 = [];
+                    var newArrBj3 = [];
+                    var newArrTf1 = [];
+                    var newArrTf2 = [];
+                    var newArrTf3 = [];
+                    $.each(data.data.data.separate_room,function(i,key){
+                        console.log(key);
+                        if(key[0].slice(0,1) == "1"){
+                            newArrDj1.push(key);
+                        }else if(key[0].slice(0,1) == "2"){
+                            newArrDj2.push(key);
+                        }else if(key[0].slice(0,1) == "3"){
+                            newArrDj3.push(key);
+                        }
+                    });
+                    $.each(data.data.data.standard_room,function(i,key){
+                        if(key[0].slice(0,1) == "1"){
+                            newArrBj1.push(key);
+                        }else if(key[0].slice(0,1) == "2"){
+                            newArrBj2.push(key);
+                        }else if(key[0].slice(0,1) == "3"){
+                            newArrBj3.push(key);
+                        }
+                    }); 
+                    $.each(data.data.data.suite,function(i,key){
+                        if(key[0].slice(0,1) == "1"){
+                            newArrTf1.push(key);
+                        }else if(key[0].slice(0,1) == "2"){
+                            newArrTf2.push(key);
+                        }else if(key[0].slice(0,1) == "3"){
+                            newArrTf3.push(key);
+                        }
+                    });
+                    console.log(selVal);
+                    if(selVal == 1){
+                        that.roomsType[0].roomsLst = newArrDj1;
+                        that.roomsType[1].roomsLst = newArrBj1;
+                        that.roomsType[2].roomsLst = newArrTf1;
+                    }else if(selVal == 2){
+                        that.roomsType[0].roomsLst = newArrDj2;
+                        that.roomsType[1].roomsLst = newArrBj2;
+                        that.roomsType[2].roomsLst = newArrTf2;
+                    }else if(selVal == 3){
+                        that.roomsType[0].roomsLst = newArrDj3;
+                        that.roomsType[1].roomsLst = newArrBj3;
+                        that.roomsType[2].roomsLst = newArrTf3;
+                    }else{
+                        that.roomsType[0].roomsLst = data.data.data.separate_room;
+                        that.roomsType[1].roomsLst = data.data.data.standard_room;
+                        that.roomsType[2].roomsLst = data.data.data.suite;                    
+                    }
+
+    
                 }, function(data){
                     // 响应错误回调
                 });
