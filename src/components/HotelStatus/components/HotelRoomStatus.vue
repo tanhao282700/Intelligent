a2ca697de61b7138bb653b9caabed448b867fedd
yyys <yangshuzhong6@163.com>
2018-11-07 23:14:46 +0800

    bug

--- src/components/HotelStatus/components/HotelRoomStatus.vue
+++ src/components/HotelStatus/components/HotelRoomStatus.vue
@@ -27,7 +27,7 @@
 
                     <el-dropdown trigger="click">
                         <span class="el-dropdown-link">{{onItemFloor}}</span>
-                        <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-menu slot="dropdown" class="hotelStaDropDown">
                             <el-dropdown-item  v-for="(item,index) in hotelFloors" :key="item.id" divided>
                                 <div @click="changeItemId(item.id,item.title)"><b :class="index==activeIndex ? 'bottom' : 'left'" @click.stop="toggleHdOn(index)"></b>{{item.title}} <input :value="item.id"/></div>
                                 <div v-show="activeIndex===index" class="itemChild" v-for="sItem in item.child" @click="changeItemId(sItem.id,sItem.title)">{{sItem.title}} <input :value="sItem.id"/></div>
