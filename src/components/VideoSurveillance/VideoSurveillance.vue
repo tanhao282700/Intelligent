ed2b3d64ebfc035e963bcddb9bb6fe37957fd686
yyys <yangshuzhong6@163.com>
2018-11-12 23:12:42 +0800

    mj

--- src/components/VideoSurveillance/VideoSurveillance.vue
+++ src/components/VideoSurveillance/VideoSurveillance.vue
@@ -136,10 +136,9 @@
 	        };
 	    },
         mounted(){
-            this.getData("115");
+            this.getData("166");
             this.getbuildData();
             this.getDateSet();
-            this.getVideoCount(this.onFloorId);
             window.addEventListener('message', this.handleMessage)
             this.iframeWin = this.$refs.iframe.contentWindow
         },
@@ -190,7 +189,6 @@
                 this.buildDatas1 = arrL;
                 this.onFloorId = selVal;
                 this.getData(selVal);
-                this.getVideoCount(selVal);
             },
             chooseBuild1(selVal){
                 var arrLs=[];
@@ -202,7 +200,6 @@
                 this.selectedValue2 = '';
                 // this.buildDatas2 = arrLs;
                 this.getData(selVal);
-                this.getVideoCount(selVal);
             },
             getData(flId){
                 console.log(this.$store.state);
@@ -216,6 +213,11 @@
                     //响应成功回调
                     that.iList = data.data.data.object_device;
                     // that.iframeSrc = data.data.data.object_3d
+                    $.each(data.data.data,(i,k)=>{
+                        if(k.floor_id == flId){
+                            that.totalVdeoNum = k.object_device.length;
+                        }
+                    });
                     console.log(data);
                 }, function(data){
                     // 响应错误回调
@@ -238,38 +240,8 @@
                 }, function(data){
                     // 响应错误回调
                 });
-
-                // $.each(this.floorObjs,function(i,k){
-                //     $.each(k.object_device,function(i1,k1){
-                //          if(k1.object_id == deviceId) {
-                //              console.log(k1.device_id)
-                //              console.log(k1.object_id)
-                //              console.log(k1.state)
-                //          }
-                //     });
-                // });
-
-                // this.videoNow();
             },
-            videoNow(){
-                if (flvjs.isSupported()) {
-                    var videoElement = document.getElementById('videoElement');
-                    var flvPlayer = flvjs.createPlayer({
-                            type: 'flv',
-                            isLive: true,
-                            enableStashBuffer:true,
-                            hasAudio: false,
-                            stashInitialSize: 384,
-                            hasVideo: true,
-                            enableWorker: true,
-                            autoCleanupSourceBuffer: true,
-                            url: 'ws://120.76.20.236:8081/action=stream,project=14,camera=25??'
-                    });
-                    flvPlayer.attachMediaElement(videoElement);
-                    flvPlayer.load();
-                    flvPlayer.play();
-                }
-            },
+
             getbuildData(){
                 console.log(this.$store.state);
                 var that = this;
@@ -290,26 +262,6 @@
                     // 响应错误回调
                 });
             },
-	    	// viewLiveVideo(id){
-	    	// 	this.isVideoShowBoxShow = true;
-	    	// 	this.onVideoId = id;
-
-
-      //           var that = this;
-      //           this.$http.post('/video_monitoring/video_view_details',{
-      //               device_id:id,
-      //           }).then(function(data){
-      //               //响应成功回调
-      //               // console.log(data.data.data);
-      //               that.videoPanelBox = data.data.data.device_state_pic;
-                  
-      //               // that.floorBgImg = data.data.data.entrance_guard_info.floor_background;
-      //           }, function(data){
-      //               // 响应错误回调
-      //           });
-
-
-	    	// },
 	    	deviceInfoGet(id){
 	    		this.isDeviceInfoPopShow = true;
 	    		
@@ -337,23 +289,6 @@
                 }
                 this.currentView = tabText;
             },
-            getVideoCount(flId){
-
-                var that = this;
-                this.$http.post('/video_monitoring/video_device_count',{
-                    sys_menu_id:this.$store.state.sysList['5'].sys_menu_id,
-                    floor_id:flId,
-                }).then(function(data){
-                    //响应成功回调
-                    console.log(data.data.data);
-                    that.totalVdeoNum = data.data.data[0].device_total;
-                  
-                    // that.floorBgImg = data.data.data.entrance_guard_info.floor_background;
-                }, function(data){
-                    // 响应错误回调
-                });
-
-            }
 	    }
     }
 </script>
